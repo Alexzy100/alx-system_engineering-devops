8b683179e8cef8b67289c9a4d8803efdc9868b2f
Alexzy100 <fineb500@gmail.com>
2023-07-16 13:20:39 -0700

    .

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <unistd.h>
 
 /**
- * main - Prints the message "and that piece of art is useful" - Dora Korpar, 2015-10-19", 
- * follow by newline,standard error.
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", 
+ * follow by a newline, to standard error.
  * Return: Always 1.
  */
 
 
 int main(void)
+
 {
  
     write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
