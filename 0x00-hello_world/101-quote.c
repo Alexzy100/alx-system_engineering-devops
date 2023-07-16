0f017b9177a7b48210e9b9203f72b4e7aeb3a1a2
Alexzy100 <fineb500@gmail.com>
2023-07-16 13:23:54 -0700

    .

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,15 @@
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", 
- * follow by a newline, to standard error.
- * Return: Always 1.
+ * main - Entry point of the program
+ *
+ * Description: Prints the message "and that piece of art is useful"
+ *              - Dora Korpar, 2015-10-19 to the standard error.
+ *
+ * Return: Always 1 (Error exit status)
  */
-
-
 int main(void)
-
 {
- 
-    write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-    return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
-
